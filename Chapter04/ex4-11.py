7195bd6d320af36a4eceff85b6291106dfe08ced
itc-s25007 <s25007@std.it-college.ac.jp>
2025-05-22 04:59:08 +0000

    P40-68 第４章終了

---
+++ Chapter04/ex4-11.py
@@ -0,0 +1,12 @@
+def even_odd():
+    n=input("n is even.")
+    n=int(n)
+    if n%2==0:
+        print("n is even.")
+    else:
+        print("n is odd.")
+
+even_odd()
+even_odd()
+even_odd()
+
---
+++ Chapter04/ex4-19.py
@@ -0,0 +1,5 @@
+a=input("type a number.")
+b=input("type aanother.")
+a=int(a)
+b=int(b)
+print(a/b)
