a5a7ae0d557617b62e5c7a91f599fb6ba5615969
itc-s25007 <s25007@std.it-college.ac.jp>
2025-05-29 03:43:07 +0000

    P88-P101 第６章演習終了

---
+++ Chapter06/ex6-08.py
@@ -0,0 +1,23 @@
+"""
+文字列をすべて大文字にするには、upperメソッドを使います。
+
+        upperメソッド=.upper()
+"""
+
+print("We hold these truths...".upper())
+
+"""
+文字列をすべて小文字にするには、lowerメソッドを使います。
+        
+        lowerメソッド=.lower()
+"""
+
+print("SO IT GOES.".lower())
+
+"""
+文字列の最初の文字を大文字で書くことをキャピタライズといいます。capitalizeメソッドを使えば文字列の先頭だけを大文字に、残りを小文字に変換できます。
+        
+        capitalizeメソッド=.capitalize
+"""
+
+print("four score and...".capitalize())
